51efa76ba7557eb1c95f778237527585cdd901d7
lorinanthony <lcrawford@microsoft.com>
2022-07-20 12:19:35 -0400

    Cleaning Functions

--- compute_ld_1000G.py
+++ compute_ld_1000G.py
@@ -1,12 +1,7 @@
 """
-<<<<<<< HEAD
 Implements fast calculation of MELD scores
 Gregory Darnell and Samuel Pattillo Smith
 Extended from initial script written by Shadi Zabad, April 2020
-=======
-Author: Shadi Zabad
-Date: April 2020
->>>>>>> fb81e9ae3b27ac48cbf5a2ee1e940d56330c8771
 """
 
 import numpy as np
@@ -57,8 +52,7 @@ def read_plink_files(input_fname, chr_num):
     gt_meta = gt_meta[['CHR', 'SNP', 'CM', 'BP', 'MAF']]
 
     if weights:
-        #sel_snp_idx = np.where(gt_meta['SNP'].isin(snp_list))[0]
-        sel_snp_idx = gt_meta.index
+        sel_snp_idx = np.where(gt_meta['SNP'].isin(snp_list.squeeze()))[0]
 
         fgt_meta = gt_meta.iloc[sel_snp_idx, ].reset_index(drop=True)
         fgt_ac = gt_ac[:, sel_snp_idx]
@@ -184,17 +178,12 @@ def compute_modified_ld_score(j, max_cm_dist=1.):
                 np.dot((neighb_snps_annot * (var_xk.reshape(-1, 1) ** (1. - lds['alpha']))).T,
                        uncr_r2)
             )
-<<<<<<< HEAD
-        # else:
-        #     raise Exception(f"LD estimator {lds['estimator']} not implemented!")
-=======
         else:
             raise Exception(f"LD estimator {lds['estimator']} not implemented!")
->>>>>>> fb81e9ae3b27ac48cbf5a2ee1e940d56330c8771
 
     return j, scores
 
-def compute_modified_meld_score(j, win_size, max_cm_dist=1.):
+def compute_meld_score(j, win_size, max_cm_dist=1.):
 
     # Obtain neighboring SNPs information:
     # --------------------------------------------
@@ -202,7 +191,7 @@ def compute_modified_meld_score(j, win_size, max_cm_dist=1.):
     neighb_snps = gt_meta.loc[(np.abs(gt_meta['CM'] - gt_meta.iloc[j, ]['CM']) <= max_cm_dist), ]
     neighb_snps = neighb_snps.drop_duplicates(subset = ['SNP'],keep = 'first')
     neighb_snps = neighb_snps.reset_index(drop = True)
-    # exclude focal snp below
+    # condition to exclude focal snp below
     #neighb_snps = gt_meta.loc[(np.abs(gt_meta['CM'] - gt_meta.iloc[j, ]['CM']) <= max_cm_dist) \
     #                          & (gt_meta.index != gt_meta.iloc[j,].name), ]
 
@@ -210,13 +199,7 @@ def compute_modified_meld_score(j, win_size, max_cm_dist=1.):
     # reduce annotation by 1 column for MELD
     neighb_snps_annot = neighb_snps_annot[:,0][:,None]
 
-    
-
     neighb_snps_idx = neighb_snps.index.values
-<<<<<<< HEAD
-=======
-    print(neighb_snps_idx)
->>>>>>> fb81e9ae3b27ac48cbf5a2ee1e940d56330c8771
 
     var_xk = neighb_snps['VAR'].values
     var_xj = gt_meta.iloc[j, ]['VAR']
@@ -243,32 +226,24 @@ def compute_modified_meld_score(j, win_size, max_cm_dist=1.):
     scores = [] # list of numpy arrays of shape (1 x n_annot)
 
     # COMPUTE MELD SCORES
-    #meld_half_win = 10 # 10 SNPs
     meld_half_win = int(win_size/2)
 
     min_win = max(0,j - meld_half_win)
     max_win = min(len(gt_meta)-1,j + meld_half_win)
 
     neighb_snps_meld = gt_meta.iloc[min_win:max_win+1, ]
-    # exclude focus snp
+    # condition to exclude focal snp below
     #neighb_snps_meld = neighb_snps_meld.loc[neighb_snps_meld.index != gt_meta.iloc[j,].name]
 
     neighb_snps_idx_meld = neighb_snps_meld.index.values
     var_xk_meld = neighb_snps_meld['VAR'].values
-    var_xj_meld = gt_meta.iloc[j, ]['VAR']
 
-    # product of independent variables assumption
-    # multiply focal snp var by partner snp variances
-    #var_xk_meld = var_xj_meld * var_xk_meld
     # create W matrix
-    #uncr_r2 = (np.dot(ngt_ac[:, j], np.multiply(ngt_ac[:,j],ngt_ac[:, neighb_snps_idx])) / N)**2
     uncr_r2_meld = (np.dot(ngt_ac[:, j]**2,ngt_ac[:, neighb_snps_idx_meld]) / N)**2
     r2_meld = uncr_r2_meld - (1. - uncr_r2_meld)/(N - 2)
-    # NTODO: meld adjusted by alpha?
     r2_meld *= var_xk_meld
 
     meld_score = sum(r2_meld)
-
         
     for lds in scores_to_compute.values():
 
@@ -287,15 +262,9 @@ def compute_modified_meld_score(j, win_size, max_cm_dist=1.):
                 np.dot((neighb_snps_annot * (var_xk.reshape(-1, 1) ** (1. - lds['alpha']))).T,
                        uncr_r2)
             )
-<<<<<<< HEAD
-        # else:
-        #     raise Exception(f"LD estimator {lds['estimator']} not implemented!")
-=======
         else:
             raise Exception(f"LD estimator {lds['estimator']} not implemented!")
->>>>>>> fb81e9ae3b27ac48cbf5a2ee1e940d56330c8771
         
-        # add in meld score
         scores[-1] = np.append(scores[-1],meld_score)
 
     return j, scores
@@ -309,14 +278,11 @@ if __name__ == '__main__':
                         help='Calculate the weights for the LDSC')
     parser.add_argument('--chrom', dest='chrom', type=int)
     parser.add_argument('--win', dest='win', type=int)
-<<<<<<< HEAD
     parser.add_argument('--alpha',dest = 'alpha',type = float)
     parser.add_argument('--plink_dir',dest = 'plink_dir',type = str)
     parser.add_argument('--out',dest = 'out',type = str)
     parser.add_argument('--filter_list',dest = 'list',type = str)
     parser.add_argument('--annotations',dest = 'annotations',type = str)
-=======
->>>>>>> fb81e9ae3b27ac48cbf5a2ee1e940d56330c8771
 
     args = parser.parse_args()
     chrom = args.chrom
@@ -328,23 +294,8 @@ if __name__ == '__main__':
     annot_start_idx = 6
     weights = args.weights
 
-    #ld_estimator = ['D2', 'R2']  #, 'NR2']
-    ld_estimator = ['R2']  #, 'NR2']
-<<<<<<< HEAD
-    # print(args.alpha)
+    ld_estimator = ['R2']
     alpha = [args.alpha]
-=======
-    #alpha = [0., .25, .5, .75, 1.]
-    #alpha = [0.45, 0.24, 0.19, 0.43, 0.39,\
-    #         0.38, 0.25, 0.42, 0.52, 0.19,\
-    #         0.40]
-    #alpha = [0.37, 0.09, 0.13, 0.40, 0.11,\
-    #         0.07, 0.20, 0.41]
-    alpha = [0.45, 0.24, 0.19, 0.43, 0.39,\
-             0.38, 0.25, 0.42, 0.52,\
-             0.40, 0.37, 0.09, 0.13,\
-             0.11, 0.07, 0.20, 0.41]
->>>>>>> fb81e9ae3b27ac48cbf5a2ee1e940d56330c8771
 
     scores_to_compute = {
         lde + '_' + str(a): {
@@ -365,66 +316,25 @@ if __name__ == '__main__':
 
     # = = = = = = = = = =
     # Input:
-    #plink_dir = "./data/genotype_files/1000G_Phase3_" + population + "_plinkfiles/1000G." + population + ".QC.%s"
-    #plink_dir = "/users/gdarnel1/data/gdarnel1/1kg_data/1000G_EUR_Phase3_plink/1000G.EUR.QC.%s"
-    #w_snp_filter = "./data/genotype_files/w_snplist_no_MHC.snplist.bz2"
-    #annotations = "./data/ld_scores/1000G_Phase3_" + population + "_baselineLD_v2.2_ldscores/baselineLD.%d.annot.gz"
-
-    #Biobank japan using EAS population
-<<<<<<< HEAD
-    # plink_dir = "/users/ssmith40/data/ukbiobank_jun17/ssmith/ongoing/meld/biobankjapan/EAS_1000G/plinkfiles/EAS.final.chr%s"
     plink_dir = args.plink_dir + '%s'
-    # w_snp_filter = "/users/ssmith40/data/ukbiobank_jun17/ssmith/ongoing/meld/biobankjapan/EAS_1000G/list.txt"
     w_snp_filter = args.list
-    # annotations = "/users/ssmith40/data/ukbiobank_jun17/ssmith/ongoing/meld/biobankjapan/EAS_1000G/EAS_annotations/1000G.EAS.%d.annot"
     annotations = args.annotations + '.%d.annot'
-=======
-    plink_dir = "/users/ssmith40/data/ukbiobank_jun17/ssmith/ongoing/meld/biobankjapan/EAS_1000G/plinkfiles/EAS.final.chr%s"
-    w_snp_filter = "/users/ssmith40/data/ukbiobank_jun17/ssmith/ongoing/meld/biobankjapan/EAS_1000G/list.txt"
-    annotations = "/users/ssmith40/data/ukbiobank_jun17/ssmith/ongoing/meld/biobankjapan/EAS_1000G/EAS_annotations/1000G.EAS.%d.annot"
 
->>>>>>> fb81e9ae3b27ac48cbf5a2ee1e940d56330c8771
-    # 1kg full dataset
-    # plink_dir = "/users/gdarnel1/data/gdarnel1/1kg_data/1000G_EUR_Phase3_plink/1000G.EUR.QC.%s"
-    # w_snp_filter = "/users/gdarnel1/data/gdarnel1/1kg_data/hapmap3_snps/list.txt"
-    # annotations = "/users/gdarnel1/data/gdarnel1/1kg_data/1000G_EUR_Phase3_plink/1000G.EUR.QC.%d.annot"
-    # 1kg subsetted to UKB
-    #plink_dir = "/users/gdarnel1/data/gdarnel1/1kg_data/1000G_EUR_Phase3_plink/1000G.UKB.SNPS.%s"
-    #w_snp_filter = "/users/gdarnel1/data/gdarnel1/1kg_data/hapmap3_snps/list.txt"
-    #annotations = "/users/gdarnel1/data/gdarnel1/1kg_data/1000G_EUR_Phase3_plink/1000G.UKB.SNPS.%d.annot"
-
-    # = = = = = = = = = =
-    # Output:
-    #output_dir = "./output/ld_scores%s/1000G_Phase3_%s_mldscores/" % (['', '_weights'][weights], population)
-    #output_dir = "/users/gdarnel1/data/gdarnel1/1kg_data/ldscores%s/1000G_Phase3_%s_mldscores/LD_" % (['', '_weights'][weights], population)
-    #output_dir = "/users/gdarnel1/data/gdarnel1/1kg_data/ldscores%s/1000G_Phase3_%s_mldscores/Full_LD_" % (['', '_weights'][weights], population)
-    # for meld:
-    #output_dir = "/users/gdarnel1/data/gdarnel1/1kg_data/ldscores%s/1000G_Phase3_%s_mldscores/MELD_" % (['', '_weights'][weights], population)
-    # output_dir = "/users/gdarnel1/data/gdarnel1/1kg_data/ldscores%s/1000G_Phase3_%s_mldscores/Full_MELD_" % (['', '_weights'][weights], population)
-<<<<<<< HEAD
-    # output_dir = "/users/ssmith40/data/ukbiobank_jun17/ssmith/ongoing/meld/biobankjapan/EAS_1000G/EAS_annotations/outputs/"
     output_dir = args.out
-=======
-    output_dir = "/users/ssmith40/data/ukbiobank_jun17/ssmith/ongoing/meld/biobankjapan/EAS_1000G/EAS_annotations/outputs/"
->>>>>>> fb81e9ae3b27ac48cbf5a2ee1e940d56330c8771
     output_dirs = [os.path.join(output_dir, sn) for sn in scores_to_compute]
     [makedir(od) for od in output_dirs]
 
     # = = = = = = = = = =
     # Read the snp filter file:
-    #try:
-    #    snp_list = pd.read_csv(w_snp_filter, sep="\t")['SNP'].values
-    #except Exception as e:
-    #    raise e
+    snp_filter = False
+    if not w_snp_filter == None:
+        snp_list = pd.read_csv(w_snp_filter, sep="\t")['SNP'].values
+        snp_filter = True
 
     # ---------------------------------------------------------
 
-    #for chr_num in range(22, 0, -1):
-    #for chr_num in range(22, 21, -1):
     for chr_num in range(chrom, chrom+1):
 
-        #output_files = [os.path.join(od, "LD.%s.l2.ldscore" % str(chr_num)) for od in output_dirs]
-        ###output_files = [os.path.join(od, "MELD.%s.l2.ldscore" % str(chr_num)) for od in output_dirs]
         output_files = [os.path.join(od, f"MELD.win_{win}.{str(chr_num)}.l2.ldscore") for od in output_dirs]
 
         print("Processing chromosome %s..." % str(chr_num))
@@ -445,10 +355,6 @@ if __name__ == '__main__':
         else:
             try:
                 annot_df = pd.read_csv(annotations % chr_num, sep="\s+").drop(['CHR', 'BP', 'CM'], axis=1)
-                # comment line below to generate MELD scores
-                # UNcomment for regular LD scores only
-                # just changing the annotation matrix
-                #annot_df = annot_df.iloc[:,:-1]
                 gt_meta = pd.merge(gt_meta, annot_df, on='SNP')
             except Exception as exp:
                 gt_meta['base'] = 1.
@@ -456,25 +362,18 @@ if __name__ == '__main__':
         output_colnames = [[cn + sn for cn in gt_meta.columns[annot_start_idx:]]
                            for sn in scores_to_compute]
 
-        # -------------------------------------------------
-
-<<<<<<< HEAD
-        # print(M, N)
-=======
-        print(M, N)
->>>>>>> fb81e9ae3b27ac48cbf5a2ee1e940d56330c8771
         print("Computing LD Scores...")
 
         if not weights:
             alphas = [scores_to_compute[sn]['alpha'] for sn in scores_to_compute]
             for idx,of in enumerate(output_files):
                 alpha = 1.0 - alphas[idx]
+                # when using an \alpha parameter, need to correct .M files
+                # with total number of SNPs corrected by variance^alpha
+                # in order for LDSC heritabilities to be computed correctly
                 M_tot = gt_meta.iloc[:,annot_start_idx:].multiply(gt_meta["VAR"],axis="index")
                 M_tot = M_tot**alpha
                 M_tot = M_tot.sum(axis=0).values
-                #M_tot = gt_meta.iloc[:, annot_start_idx:].sum(axis=0).values
-                # new sum for .M file
-                #M_5_50 = gt_meta.loc[gt_meta['MAF'] >= .05, ].iloc[:, annot_start_idx:].sum(axis=0).values
                 M_5_50 = gt_meta.loc[gt_meta['MAF'] >= .05, ].iloc[:, annot_start_idx:].multiply(gt_meta["VAR"],axis="index")
                 M_5_50 = M_5_50**alpha
                 M_5_50 = M_5_50.sum(axis=0).values
@@ -493,18 +392,15 @@ if __name__ == '__main__':
             cw.writerow(list(gt_meta.columns[:annot_start_idx - 1]) + col)
 
         # Select the subset of snps to compute the the LD scores for:
-        #snps_to_process = list(np.where(gt_meta['SNP'].isin(snp_list))[0])
-        snps_to_process = gt_meta.index
+        if snp_filter:
+            snps_to_process = list(np.where(gt_meta['SNP'].isin(snp_list.squeeze()))[0])
+        else:
+            snps_to_process = gt_meta.index
 
         # Compute the LD Scores:
-        #for idx, (snp_idx, ld_scores) in enumerate(pool.imap(compute_modified_ld_score, snps_to_process), 1):
-        #####for idx, (snp_idx, ld_scores) in enumerate(pool.imap(compute_modified_meld_score, snps_to_process), 1):
-        # last parameter (1) is max distance for ld score
+        # last parameter (1) is the distance for ld score neighbors (in cM)
         for idx, (snp_idx, ld_scores) \
-                in enumerate(pool.starmap(compute_modified_meld_score, product(snps_to_process, [win], [1])), 1):
-        #for idx in snps_to_process:
-            #snp_idx, ld_scores = compute_modified_ld_score(idx)
-            #snp_idx, ld_scores = compute_modified_meld_score(idx)
+                in enumerate(pool.starmap(compute_meld_score, product(snps_to_process, [win], [1])), 1):
 
             for cw, lds in zip(csv_writers, ld_scores):
                 cw.writerow(list(gt_meta.iloc[snp_idx, :annot_start_idx - 1]) +
